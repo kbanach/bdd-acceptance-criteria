fb89ab92207272f7295b480f8eaa6018b430f77f
Krystian Banach <banach.krystian@gmail.com>
2022-03-19 10:16:16 +0100

    chore: add create-react-app bootstrap

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# bdd-acceptance-criteria
+Tool for easy create and copy-paste BDD acceptance criteria.
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Lorem ipsum
+    </div>
+  );
+}
+
+export default App;
