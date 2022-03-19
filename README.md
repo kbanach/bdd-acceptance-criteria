03d91b29295b2c8b2a7b284b89306bdf49ae574a
Krystian Banach <banach.krystian@gmail.com>
2022-03-19 11:00:36 +0100

    chore: add MUI library

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+## Layout
+
+Project is using [MUI](https://mui.com/)  for layout.
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
+import CssBaseline from '@mui/material/CssBaseline';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <App />
+    </ThemeProvider>,
   </React.StrictMode>,
   document.getElementById('root')
 );
